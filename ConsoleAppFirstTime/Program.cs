89b483d53d2d8f2912b58ea91ffc99a2d725c7a2
didrika@protonmail.com <didrika@protonmail.com>
2020-09-07 03:21:07 -0700

    Added  user input for numbers

--- ConsoleAppFirstTime/Program.cs
+++ ConsoleAppFirstTime/Program.cs
@@ -7,6 +7,24 @@ namespace ConsoleAppFirstTime
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string input = "";
+            while (input.ToLower() != "exit")
+            {
+                Console.Write("\nWhat is your number? ");
+                input = Console.ReadLine();
+                int num;
+                if (int.TryParse(input, out num))
+                {
+                    //num = Convert.ToInt32(input);
+                    Console.WriteLine(num > 5 ? "Greater than 5." 
+                                              : num == 5 ? "Equals to 5" 
+                                                         : "Lesser then 5.");
+
+                } else if(input != "exit"){
+                    Console.WriteLine($"Error: Unable to convert \"{input}\" to int32.");
+                }
+
+            }
         }
     }
 }
